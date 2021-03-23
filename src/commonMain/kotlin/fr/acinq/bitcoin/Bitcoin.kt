add8e0ee790f0fa77d5fbf7cfff101aece5ed26f
Fabrice Drouin <sstone@users.noreply.github.com>
2021-03-23 16:15:33 +0100

    Add method to compute addresses from public key scripts (#17)

--- src/commonMain/kotlin/fr/acinq/bitcoin/Bitcoin.kt
+++ src/commonMain/kotlin/fr/acinq/bitcoin/Bitcoin.kt
@@ -17,6 +17,7 @@
 package fr.acinq.bitcoin
 
 import kotlin.jvm.JvmField
+import kotlin.jvm.JvmStatic
 
 @JvmField
 public val MaxScriptElementSize: Int = 520
@@ -38,27 +39,80 @@ public fun <T> List<T>.updated(i: Int, t: T): List<T> = when (i) {
     else -> this.take(i) + t + this.take(this.size - i - 1)
 }
 
-public fun computeP2PkhAddress(pub: PublicKey, chainHash: ByteVector32): String  = pub.p2pkhAddress(chainHash)
+public object Bitcoin {
+    @JvmStatic
+    public fun computeP2PkhAddress(pub: PublicKey, chainHash: ByteVector32): String = pub.p2pkhAddress(chainHash)
 
-public fun computeBIP44Address(pub: PublicKey, chainHash: ByteVector32): String = computeP2PkhAddress(pub, chainHash)
+    @JvmStatic
+    public fun computeBIP44Address(pub: PublicKey, chainHash: ByteVector32): String = computeP2PkhAddress(pub, chainHash)
 
-/**
- *
- * @param pub       public key
- * @param chainHash chain hash (i.e. hash of the genesic block of the chain we're on)
- * @return the p2swh-of-p2pkh address for this key). It is a Base58 address that is compatible with most bitcoin wallets
- */
-public fun computeP2ShOfP2WpkhAddress(pub: PublicKey, chainHash: ByteVector32): String = pub.p2shOfP2wpkhAddress(chainHash)
+    /**
+     *
+     * @param pub       public key
+     * @param chainHash chain hash (i.e. hash of the genesic block of the chain we're on)
+     * @return the p2swh-of-p2pkh address for this key). It is a Base58 address that is compatible with most bitcoin wallets
+     */
+    @JvmStatic
+    public fun computeP2ShOfP2WpkhAddress(pub: PublicKey, chainHash: ByteVector32): String = pub.p2shOfP2wpkhAddress(chainHash)
 
-public fun computeBIP49Address(pub: PublicKey, chainHash: ByteVector32): String = computeP2ShOfP2WpkhAddress(pub, chainHash)
+    @JvmStatic
+    public fun computeBIP49Address(pub: PublicKey, chainHash: ByteVector32): String = computeP2ShOfP2WpkhAddress(pub, chainHash)
 
-/**
- *
- * @param pub       public key
- * @param chainHash chain hash (i.e. hash of the genesic block of the chain we're on)
- * @return the BIP84 address for this key (i.e. the p2wpkh address for this key). It is a Bech32 address that will be
- *         understood only by native sewgit wallets
- */
-public fun computeP2WpkhAddress(pub: PublicKey, chainHash: ByteVector32): String = pub.p2wpkhAddress(chainHash)
+    /**
+     *
+     * @param pub       public key
+     * @param chainHash chain hash (i.e. hash of the genesic block of the chain we're on)
+     * @return the BIP84 address for this key (i.e. the p2wpkh address for this key). It is a Bech32 address that will be
+     *         understood only by native sewgit wallets
+     */
+    @JvmStatic
+    public fun computeP2WpkhAddress(pub: PublicKey, chainHash: ByteVector32): String = pub.p2wpkhAddress(chainHash)
 
-public fun computeBIP84Address(pub: PublicKey, chainHash: ByteVector32): String = computeP2WpkhAddress(pub, chainHash)
+    @JvmStatic
+    public fun computeBIP84Address(pub: PublicKey, chainHash: ByteVector32): String = computeP2WpkhAddress(pub, chainHash)
+
+    @JvmStatic
+    public fun addressFromPublicKeyScript(chainHash: ByteVector32, pubkeyScript: List<ScriptElt>): String? {
+        return try {
+            when {
+                Script.isPay2pkh(pubkeyScript) -> {
+                    val prefix = when (chainHash) {
+                        Block.LivenetGenesisBlock.hash -> Base58.Prefix.PubkeyAddress
+                        Block.TestnetGenesisBlock.hash, Block.RegtestGenesisBlock.hash -> Base58.Prefix.PubkeyAddressTestnet
+                        else -> error("invalid chain hash")
+                    }
+                    Base58Check.encode(prefix, (pubkeyScript[2] as OP_PUSHDATA).data)
+                }
+                Script.isPay2sh(pubkeyScript) -> {
+                    val prefix = when (chainHash) {
+                        Block.LivenetGenesisBlock.hash -> Base58.Prefix.ScriptAddress
+                        Block.TestnetGenesisBlock.hash, Block.RegtestGenesisBlock.hash -> Base58.Prefix.ScriptAddressTestnet
+                        else -> error("invalid chain hash")
+                    }
+                    Base58Check.encode(prefix, (pubkeyScript[1] as OP_PUSHDATA).data)
+                }
+                Script.isPay2wpkh(pubkeyScript) || Script.isPay2wsh(pubkeyScript) -> {
+                    val hrp = when (chainHash) {
+                        Block.LivenetGenesisBlock.hash -> "bc"
+                        Block.TestnetGenesisBlock.hash -> "tb"
+                        Block.RegtestGenesisBlock.hash -> "bcrt"
+                        else -> error("invalid chain hash")
+                    }
+                    Bech32.encodeWitnessAddress(hrp, 0, (pubkeyScript[1] as OP_PUSHDATA).data.toByteArray())
+                }
+                else -> null
+            }
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    @JvmStatic
+    public fun addressFromPublicKeyScript(chainHash: ByteVector32, pubkeyScript: ByteArray): String? {
+        return try {
+            addressFromPublicKeyScript(chainHash, Script.parse(pubkeyScript))
+        } catch (e: Exception) {
+            null
+        }
+    }
+}

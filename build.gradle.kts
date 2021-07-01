9dcf89d04aa9b66bb6c8e446665bd529345fdfa1
Fabrice Drouin <sstone@users.noreply.github.com>
2021-07-01 21:31:36 +0200

    Set version to 0.7.1 (#28)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 val currentOs = org.gradle.internal.os.OperatingSystem.current()
 
 group = "fr.acinq.bitcoin"
-version = "0.7.1-SNAPSHOT"
+version = "0.7.1"
 
 repositories {
     mavenLocal()

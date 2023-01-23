e1b221f7ff39aff2a692375fbad0aa66c0d565b3
Fabrice Drouin <sstone@users.noreply.github.com>
2023-01-23 17:29:18 +0100

    Set version to 0.11.0 (#81)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 val currentOs = org.gradle.internal.os.OperatingSystem.current()
 
 group = "fr.acinq.bitcoin"
-version = "0.11.0-SNAPSHOT"
+version = "0.11.0"
 
 repositories {
     google()

696affc53dee968f1ac1633927eafbdf8a69a763
Fabrice Drouin <sstone@users.noreply.github.com>
2023-03-02 14:51:48 +0100

    Fix OP_PUSHDATA encoding (#85)
    There was an error encoding pushes of 255 bytes

--- src/commonTest/kotlin/fr/acinq/bitcoin/BtcSerializerTestsCommon.kt
+++ src/commonTest/kotlin/fr/acinq/bitcoin/BtcSerializerTestsCommon.kt
@@ -5,6 +5,8 @@ import fr.acinq.bitcoin.io.ByteArrayOutput
 import fr.acinq.secp256k1.Hex
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
 
 class BtcSerializerTestsCommon {
     @Test
@@ -35,6 +37,42 @@ class BtcSerializerTestsCommon {
             assertEquals(BtcSerializer.varint(input), v - 1uL)
             assertEquals(BtcSerializer.varint(input), v)
             assertEquals(BtcSerializer.varint(input), v + 1uL)
+        }
+    }
+
+    @Test
+    fun `encode and decoded OP_PUSHDATA`() {
+        val script = listOf(OP_PUSHDATA(ByteArray(255) { 0x01 }))
+        val encoded = Script.write(script)
+        val decoded = Script.parse(encoded)
+        assertEquals(script, decoded)
+
+        fun serde(push: OP_PUSHDATA) {
+            assertEquals(listOf(push), Script.parse(Script.write(listOf(push))))
+        }
+        listOf(
+            OP_PUSHDATA(ByteArray(0x01) { 0x01 }, 0x01),
+            OP_PUSHDATA(ByteArray(0x01) { 0x01 }, 0x4c), // non-minimal encoding
+            OP_PUSHDATA(ByteArray(0x01) { 0x01 }, 0x4d), // non-minimal encoding
+            OP_PUSHDATA(ByteArray(0x01) { 0x01 }, 0x4e), // non-minimal encoding
+            OP_PUSHDATA(ByteArray(0xff) { 0x01 }, 0x4c),
+            OP_PUSHDATA(ByteArray(0xff) { 0x01 }, 0x4d), // non-minimal encoding
+            OP_PUSHDATA(ByteArray(0xffff) { 0x01 }, 0x4e), // non-minimal encoding
+        ).forEach { serde(it) }
+
+        listOf("04deadbeef", "4c04deadbeef", "4d0400deadbeef", "4e04000000deadbeef").forEach { // all encode "deadbeef"
+            val parsed = Script.parse(it)
+            assertEquals(1, parsed.size)
+            assertTrue(ScriptElt.isPush(parsed.first(), 4))
+        }
+
+        // invalid pushes
+        listOf(
+            OP_PUSHDATA(ByteArray(32) { 0x01 }, 0x31), // opCode != size
+            OP_PUSHDATA(ByteArray(0xff + 1) { 0x01 }, 0x4c), // too big
+            OP_PUSHDATA(ByteArray(0xffff + 1) { 0x01 }, 0x4d), // too big
+        ).forEach {
+            assertFailsWith<IllegalArgumentException> { Script.write(listOf(it)) }
         }
     }
 }

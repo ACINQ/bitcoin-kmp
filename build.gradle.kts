5a026010fb9eb1c0d06640e391f7709d4096241a
Fabrice Drouin <sstone@users.noreply.github.com>
2023-03-10 09:18:59 +0100

    Use secp256k1-kmp 0.8.0 (#87)

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ kotlin {
     }
 
     sourceSets {
-        val secp256k1KmpVersion = "0.7.1"
+        val secp256k1KmpVersion = "0.8.0"
 
         val commonMain by getting {
             dependencies {

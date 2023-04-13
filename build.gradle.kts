3bf54c2009874bb570cda5cf508ba5fa0c4547da
Fabrice Drouin <sstone@users.noreply.github.com>
2023-04-13 14:33:24 +0200

    Use secp256k1-kmp 0.9.0 (#91)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 val currentOs = org.gradle.internal.os.OperatingSystem.current()
 
 group = "fr.acinq.bitcoin"
-version = "0.11.1"
+version = "0.12.0-SNAPSHOT"
 
 repositories {
     google()
@@ -37,7 +37,7 @@ kotlin {
     }
 
     sourceSets {
-        val secp256k1KmpVersion = "0.8.0"
+        val secp256k1KmpVersion = "0.9.0"
 
         val commonMain by getting {
             dependencies {

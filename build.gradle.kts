51c5c3bd91cb6a52c5ca7b1afa6c1449ca2a3f45
sstone <fabrice.drouin@acinq.fr>
2023-05-12 10:12:49 +0200

    Set version to 0.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 val currentOs = org.gradle.internal.os.OperatingSystem.current()
 
 group = "fr.acinq.bitcoin"
-version = "0.12.0-SNAPSHOT"
+version = "0.12.0"
 
 repositories {
     google()

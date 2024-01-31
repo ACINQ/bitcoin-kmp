e3ba13542eb42a7e93c2ca572767967179955986
sstone <fabrice.drouin@acinq.fr>
2024-01-31 16:52:29 +0100

    Set version back to 0.17.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 val currentOs = org.gradle.internal.os.OperatingSystem.current()
 
 group = "fr.acinq.bitcoin"
-version = "0.17.0-EITHER-SNAPSHOT"
+version = "0.17.0-SNAPSHOT"
 
 repositories {
     google()

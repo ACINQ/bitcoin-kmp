0faae6572b17892c8bcfa0176509f540bbfd0fd6
Fabrice Drouin <sstone@users.noreply.github.com>
2025-06-26 11:55:11 +0200

    Upgrade build and publishing scripts (#154)
    Upgrade to Kotlin 2.1.21.
    Upgrade to secp256k1-kmp 0.18.0.
    Migrate publishing scripts from OSSRH to Central Publisher API.

--- publishing/bitcoin-kmp-staging-upload.sh
+++ publishing/bitcoin-kmp-staging-upload.sh
@@ -9,13 +9,15 @@ if [[ -z "${VERSION}" ]]; then
   exit 1
 fi
 
-if [[ -z "${OSS_USER}" ]]; then
-  echo "OSS_USER is not defined"
+if [[ -z "${CENTRAL_TOKEN_GPG_FILE}" ]]; then
+  echo "CENTRAL_TOKEN_GPG_FILE is not defined"
   exit 1
 fi
 
-read -p "Password : " -s OSS_PASSWORD
+CENTRAL_TOKEN=$(gpg --decrypt $CENTRAL_TOKEN_GPG_FILE)
 
+pushd .
+cd release
 for i in 	bitcoin-kmp \
 		bitcoin-kmp-iosarm64 \
 		bitcoin-kmp-iossimulatorarm64 \
@@ -26,10 +28,20 @@ for i in 	bitcoin-kmp \
 		bitcoin-kmp-macosarm64 \
 		bitcoin-kmp-macosx64
 do
-	pushd .
-	cd release/fr/acinq/bitcoin/$i/$VERSION &&\
-	pwd &&\
-	jar -cvf bundle.jar *&&\
-	curl -v -XPOST -u $OSS_USER:$OSS_PASSWORD --upload-file bundle.jar https://oss.sonatype.org/service/local/staging/bundle_upload	
-  popd
+	DIR=fr/acinq/bitcoin/$i/$VERSION
+	case $1 in
+	  create)
+  	  for file in $DIR/*
+	    do
+	      sha1sum $file | sed 's/ .*//' > $file.sha1
+	      md5sum $file | sed 's/ .*//' > $file.md5
+	      gpg -ab $file
+      done
+	    zip -r $i.zip $DIR
+	  ;;
+  	upload)
+	    curl --request POST --verbose --header "Authorization: Bearer ${CENTRAL_TOKEN}" --form bundle=@$i.zip https://central.sonatype.com/api/v1/publisher/upload
+    ;;
+  esac
 done
+popd

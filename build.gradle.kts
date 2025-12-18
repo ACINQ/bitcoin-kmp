9d0cf08c4563cebf3b43e4612032533ca06644b1
Fabrice Drouin <sstone@users.noreply.github.com>
2025-12-18 15:03:13 +0100

    Set version to 0.29.0 (#171)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 val currentOs = org.gradle.internal.os.OperatingSystem.current()
 
 group = "fr.acinq.bitcoin"
-version = "0.29.0-SNAPSHOT"
+version = "0.29.0"
 
 repositories {
     mavenCentral()

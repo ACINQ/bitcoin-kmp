8427ef60b19b0efbf3d6bcfa9f4b82e060a9f24a
sstone <fabrice.drouin@acinq.fr>
2026-06-01 10:36:16 +0200

    Set version to 0.32.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 val currentOs = org.gradle.internal.os.OperatingSystem.current()
 
 group = "fr.acinq.bitcoin"
-version = "0.31.0"
+version = "0.32.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
